2d010853968f42fc5ead13298fdf0ad8c242c681
Deeksha <deekshadaga2014@gmail.com>
2022-07-13 12:25:46 +0530

    added customer.java file

---
+++ StudentInformationSystem/src/model/Customer.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Customer {
+	int customer;
+
+	public int getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(int customer) {
+		this.customer = customer;
+	}
+	
+
+}
