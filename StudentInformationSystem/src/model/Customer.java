b6fe7735a7f6c357eff5d4aa01db0a654e8546c3
Deeksha <deekshadaga2014@gmail.com>
2022-07-14 17:26:50 +0530

    First Commit

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -2,14 +2,10 @@ package model;
 
 public class Customer {
 	int customer;
-
 	public int getCustomer() {
 		return customer;
 	}
-
-	public void setCustomer(int customer) {
+	public void setCustomer(int customer){
 		this.customer = customer;
 	}
-	
-
 }
